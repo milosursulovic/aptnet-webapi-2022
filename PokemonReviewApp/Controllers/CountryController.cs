c1443a9b99607ea20d16b49c9eab5a68dbf6da63
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-11-11 16:38:57 +0100

    Implement country controller

---
+++ PokemonReviewApp/Controllers/CountryController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMapper _mapper;
+
+        public CountryController(ICountryRepository countryRepository, IMapper mapper)
+        {
+            _countryRepository = countryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+        public IActionResult GetCountries()
+        {
+            var countries = _mapper.Map<List<CountryDto>>(_countryRepository.GetCountries());
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            return Ok(countries);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountry(int id)
+        {
+            if (!_countryRepository.CountryExists(id))
+                return NotFound();
+            var country = _mapper.Map<CountryDto>(_countryRepository.GetCountry(id));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(country);
+        }
+
+        [HttpGet("owners/{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountryOfAnOwner(int ownerId)
+        {
+            var country = _mapper.Map<CountryDto>(_countryRepository.GetCountryByOwner(ownerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(country);
+        }
+    }
+}
