015e7fb059f4ab187eea4c59d6584d20f22484f9
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-11-11 12:24:56 +0100

    Add more get endpoints

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddTransient<Seed>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
