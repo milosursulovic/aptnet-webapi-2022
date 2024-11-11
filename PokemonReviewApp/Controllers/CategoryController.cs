b6b3ec977d5b2423682dee30f0be4dfbbf7a561d
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-11-11 13:15:46 +0100

    Implement category controller

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCategory(int id)
+        {
+            if (!_categoryRepository.CategoriesExists(id))
+                return NotFound();
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(id));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(category);
+        }
+
+        [HttpGet("pokemon/{id}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByCategoryId(int id)
+        {
+            var pokemons = _mapper.Map<List<PokemonDto>>(_categoryRepository.GetPokemonByCategory(id));
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(pokemons);
+        }
+    }
+}
