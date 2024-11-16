49b0bf861f65b601f2cbaa963d8d14de855bae75
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-11-16 12:17:21 +0100

    Add review controller

---
+++ PokemonReviewApp/Controllers/ReviewController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class ReviewController : Controller
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewController(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _reviewRepository = reviewRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewRepository.GetReviews());
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            return Ok(reviews);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReview(int id)
+        {
+            if (!_reviewRepository.ReviewExists(id))
+                return NotFound();
+            var review = _mapper.Map<ReviewDto>(_reviewRepository.GetReview(id));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(review);
+        }
+
+        [HttpGet("pokemon/{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewForAPokemon(int pokeId)
+        {
+            var review = _mapper.Map<List<ReviewDto>>(_reviewRepository.GetReviewsOfAPokemon(pokeId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(review);
+        }
+    }
+}
